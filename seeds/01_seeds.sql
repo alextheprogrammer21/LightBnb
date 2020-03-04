8c6fdbebfb9b45b692bfae683dea39920214324b
Alex <alextheprogrammer21@gmail.com>
2020-03-04 23:16:38 +0000

    Created tables and added data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'john', 'john@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ (2, 'john', 'john@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ (3, 'john', 'john@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ (4, 'john', 'john@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ (5, 'john', 'john@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+
+-- INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+-- VALUES (1, 1, '2018-09-11', '2018-09-26'),
+-- (2, 2, '2019-01-04', '2019-02-01'),
+-- (3,3, '2021-10-01', '2021-10-14');
